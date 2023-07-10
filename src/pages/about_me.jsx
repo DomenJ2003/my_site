c9cbb67113309af94c11d1913da4240c2c1f604a
DomenJakopic <domen.jakopic2@gmail.com>
2023-07-10 19:26:48 +0200

    new text

--- src/pages/about_me.jsx
+++ src/pages/about_me.jsx
@@ -11,9 +11,7 @@ function aboutMe() {
                 <h3>About me:</h3>
                 <div style={{height: "30px"}}></div>
                 <p>
-                    I am Domen Jakopič. Software developer from Slovenia.<br />
-                    I am currently studying at the Faculty of Computer Science, Electrical Engineering and Informatics in Maribor.<br />
-                    In addition to my studies, I also work as a web developer. <br />You can also see some of my hobby projects on <Link className="link-about-me" to="/my-projects">my projects page</Link>.
+                Welcome to my profile! <br/> I'm a passionate full stack developer currently studying at FERI (Faculty of Electrical Engineering and Computer Science) since October 2022.<br/> With a focus on both front-end and back-end technologies, I create dynamic and robust applications. I've gained professional programming experience since September 2022, continuously expanding my skill set and staying up-to-date with industry trends.<br/>I enjoy tackling challenging projects and collaborating effectively with teams. Feel free to explore my profile to learn more about my skills and experiences.<br/>Let's connect and explore potential opportunities together! <br />Check out my projects page to explore some of my hobby projects. -{">"} <Link className="link-about-me" to="/my-projects">My projects page</Link>.
                 </p>
                 <div style={{height: "200px"}}></div>
             </div>
--- src/pages/my_projects.jsx
+++ src/pages/my_projects.jsx
@@ -9,19 +9,44 @@ function myProjects() {
             <div className="d-flex-collumn justify-content-center">
                 <h3 className="mt-4 text-center">Tasks App</h3>
                 <iframe src="/src/components/tasks/opravila.html" scrolling="no" style={{ border: "2px solid black", overflowX: "hidden" }} frameBorder="0" width={"100%"} height={512}></iframe>
-                <p>Tasks app is a web application. Where you can add or delete category of your task. And write new tasks or delete them. In this web application was also used localstore. This means all data stay same after refresh page. The application was made with jQuery and Bootstrap. <b>Please try it.</b></p>
+                <p>
+The Tasks app is a web application that helps you stay organized by managing your tasks. You can add or delete categories, create new tasks, and delete them. The app uses local storage, so your data remains intact even after refreshing the page. Developed with jQuery and Bootstrap, it offers a user-friendly interface and a seamless experience across devices. Stay organized and efficient with the Tasks app.<b>Please try it.</b></p>
                 <hr />
                 <h3 className="mt-3 text-center">Message App</h3>
                 <div className="row p-3 justify-content-lg-around justify-content-xl-around justify-content-md-center justify-content-sm-center" >
                     <iframe className="virtual-phone chat-phone" src="/src/components/MessageApp/index.html" scrolling="no" style={{ border: "2px solid black", overflowX: "hidden" }} frameBorder="0"></iframe>
                     <iframe className="virtual-phone chat-phone" src="/src/components/MessageApp/index.html" scrolling="no" style={{ border: "2px solid black", overflowX: "hidden" }} frameBorder="0"></iframe>
                 </div>
-                <p>Chat App is Flutter application for mobile or web. If you want to try it. Login in both devices with username: <b>test1@gmail.com</b> and password: <b>test1234</b> and another device with username: <b>test2@gmail.com</b> and password: <b>test1234</b>. You can also register a new user. In app, you can send and receive messages. if you want to start chatting with someone, you need to send them friend request. <b>Please try it.</b></p>
+                <p>Chat App is a versatile Flutter application designed for both mobile and web platforms. To try it out, follow these steps:
+                    <ol>
+                        <li>
+                            Login to the app on two different devices:
+                            <ul>
+                                <li>
+                                Device 1: Use the username "test1@gmail.com" and the password "test1234".
+                                </li>
+                                <li>
+                                Device 2: Use the username "test2@gmail.com" and the password "test1234".
+                                </li>
+                            </ul>
+                        </li>
+                        <li>
+                        Alternatively, you can register a new user within the app.
+                        </li>
+                    </ol>    
+                    Once you're logged in, you can take advantage of the following features:
+                    <ul>
+                        <li>Send and receive messages: The app allows you to exchange messages with other users.</li>
+                        <li>Initiate chats: To start a conversation with someone, send them a friend request.</li>
+                        <li>Explore the app's functionality and experience the chat feature firsthand.</li>
+                    </ul>
+                    <b>Enjoy using the Chat App to communicate and connect with others conveniently!</b>
+                </p>
                 <hr />
                 <h3 className="mt-3 text-center">Tic Tac Toe</h3>
                 <div className="row p-3 justify-content-lg-around justify-content-xl-around justify-content-md-center justify-content-sm-center">
                     <iframe className="virtual-phone" src="/src/components/TicTacToe/index.html" scrolling="no" style={{ border: "2px solid black", overflowX: "hidden" }} frameBorder="0"></iframe>
-                    <p className="ml-3 text-left">Tic Tac Toe is a mobile and web application. You have two ways to play. For one player and for two players. In single player mode, you can play against A.I. Tic Tac Toe was created with flutter framework. <b>Please try it.</b></p>
+                    <p className="ml-3 text-left">Tic Tac Toe is a mobile and web application that offers both single player and two-player modes. In single player mode, you can challenge the A.I., while in two-player mode, you can compete against a friend. Built with Flutter, it provides a seamless experience across devices. Enjoy the classic game of Tic Tac Toe and test your skills!</p>
                 </div>
             </div>
 
